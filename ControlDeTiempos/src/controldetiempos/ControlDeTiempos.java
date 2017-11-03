c088dae60537a10bfb95fa3b9b67dacc8af781c8
diego <diego@192.168.1.5>
2017-11-03 02:26:13 -0500

    Primer menu terminado

---
+++ ControlDeTiempos/src/controldetiempos/ControlDeTiempos.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controldetiempos;
+
+import Vista.IUControlTiempos;
+
+/**
+ *
+ * @author diego
+ */
+public class ControlDeTiempos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        IUControlTiempos principal = new IUControlTiempos();
+        
+        principal.setVisible(true);  
+    }
+    
+}
